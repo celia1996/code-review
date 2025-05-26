fd4c3a393c639acb511c2db04a0100c56677ec39
Celia Navarro <celiinavarroperez@gmail.com>
2025-05-26 17:42:18 +0200

    refactor: isolate CreateMessageComponent in dedicated folder

---
+++ src/app/create-message/create-message.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf, NgClass } from '@angular/common';
+import { MessageComponent } from '../message/message.component';
+import { Message } from '../message/message.model';
+import { MessageService } from '../services/message.service';
+
+@Component({
+  selector: 'app-create-message',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MessageComponent,
+    NgIf,
+    NgClass,
+  ],
+  template: `
+    <div *ngIf="!message.empty()">
+      <app-message [message]="message" no="preview"></app-message>
+    </div>
+    <form (ngSubmit)="onSubmit()">
+      <label class="mt-4">
+        <div>Write Message</div>
+        <textarea class="block w-full" required name="text" [(ngModel)]="message.text"></textarea>
+      </label>
+
+      <button type="submit"
+          [disabled]="message.status === 'pending'"
+          class="pointer bg-blue-400 py-2 px-4 mt-2 w-full"
+          [ngClass]="{'bg-gray-400': message.status === 'pending'}"
+      >Send</button>
+    </form>
+  `
+})
+export class CreateMessageComponent {
+  message: Message = new Message('', 'draft');
+
+  constructor(private messageService: MessageService) {}
+
+  async onSubmit() {
+    this.message.status = 'pending';
+
+    const res = await fetch('http://127.0.0.1:4010/messages/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: this.message.text }),
+    });
+
+    this.message.status = res.status === 204 ? 'sent' : 'failed';
+    await this.messageService.add(this.message);
+    this.message = new Message('', 'draft');
+  }
+}
